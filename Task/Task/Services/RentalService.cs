2d3fcbe12bc82fd776df5f711ac3f542ba193987
Ayaksss <s32770@pjwstk.edu.pl>
2026-03-18 18:57:55 +0100

    Implemented rent logic

--- Task/Task/Services/RentalService.cs
+++ Task/Task/Services/RentalService.cs
@@ -1,6 +1,29 @@
 namespace Task.Services;
+using Task.Users;
+using Task.Equipment;
 
 public class RentalService
 {
-    
+    List<Rental> rentals = new List<Rental>();
+
+    public void rentEquipment(User user, Equipment equipment)
+    {
+        if (equipment.isAvailable == true)
+        {
+            rentals.Add(new Rental(user, equipment));    
+            equipment.isAvailable = false;
+        }
+    }
+    public void returnItem(Equipment equipment)
+    {
+        if (equipment.isAvailable == false)
+        {
+            rentals.Remove(rentals.Find(x => x.equipment == equipment));
+            equipment.isAvailable = true;
+        }
+        else
+        {
+            Console.WriteLine(equipment + " is not rented");
+        }
+    }
 }
